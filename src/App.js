470c69d8a8ec255665e3a89a6cdd3bfbb596d3c3
Kapioga <77947921+Kapioga@users.noreply.github.com>
2025-06-07 21:02:18 -0400

    added weather api to code

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 const api = {
@@ -7,7 +7,20 @@ const api = {
 }
 
 function App() {
+  
+  // Fetch and get request from weather API
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
 
+  const search = evt => {
+    if (evt.key === "Enter") {
+      fetch(`${api.base}weather?q=${query} &units=metric&APPID=${api.key}`)
+        .then(res => res.json())
+        .then(result => setWeather(result));
+    }
+  }
+
+  // Date Handler 
   const dateBuilder = (d) => {
     let months = ["January" , "February" ,"March" , "April" ,"May" , "June" ,"July" , "August" ,"September" , "October" ,"November" , "December" ] ;
     let days = ["Sunday", "Monday","Tuesday", "Wednesday","Thursday", "Friday","Saturday" ];
@@ -35,10 +48,12 @@ function App() {
           <div className='date'>{dateBuilder(new Date())}</div>
         </div>
 
+        {/* Weather temperature  */}
         <div className='weather-box'>
           <div className='temp'> 85°C</div>
           <div className='weather'> Partly Cloud</div>
         </div>
+
       </main>
     </div>
   );
